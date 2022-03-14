557e7470cf7155c4b3ebdedd6fffaf28fb103f95
Chris Troutner <chris.troutner@gmail.com>
2022-03-14 11:13:12 -0700

    fix(create.js): Fixed incorrectly mocked unit tests

--- test/unit/create-unit.js
+++ test/unit/create-unit.js
@@ -32,7 +32,8 @@ describe('#create.js', () => {
   })
 
   afterEach(() => sandbox.restore())
-  describe('#Create', () => {
+
+  describe('#constructor', () => {
     it('should instantiate the class', async () => {
       try {
         // Mock external dependencies.
@@ -42,6 +43,7 @@ describe('#create.js', () => {
         assert.equal(true, false, 'unexpected result')
       }
     })
+
     it('should throw an error if bch-js is not provided', async () => {
       try {
         // Mock external dependencies.
@@ -53,141 +55,14 @@ describe('#create.js', () => {
       }
     })
   })
-  describe('#createToken', () => {
-    it('should create token', async () => {
-      try {
-        // Mock external dependencies.
-        sandbox
-          .stub(uut.bchjs.Utxo, 'get')
-          .resolves(mockData.mockUtxos)
 
-        sandbox
-          .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
-          .resolves(mockData.mockTxId)
-
-        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
-        const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
-
-        const slpData = {
-          name: 'SLP Test Token',
-          ticker: 'SLPTEST',
-          documentUrl: 'https://FullStack.cash',
-          decimals: 0,
-          initialQty: 1,
-          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
-          mintBatonVout: null
-        }
-        const result = await uut.createToken(WIF, slpData, mspAddr)
-        assert.isString(result)
-      } catch (err) {
-        console.log(err)
-        assert.equal(true, false, 'unexpected result')
-      }
-    })
-    it('should handle errors', async () => {
-      try {
-        // Mock external dependencies.
-        sandbox
-          .stub(uut.bchjs.Utxo, 'get')
-          .resolves(mockData.mockUtxos)
-
-        sandbox
-          .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
-          .throws(new Error('Test Error'))
-
-        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
-        const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
-
-        const slpData = {
-          name: 'SLP Test Token',
-          ticker: 'SLPTEST',
-          documentUrl: 'https://FullStack.cash',
-          decimals: 0,
-          initialQty: 1,
-          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
-          mintBatonVout: null
-        }
-        const result = await uut.createToken(WIF, slpData, mspAddr)
-        assert.isString(result)
-      } catch (err) {
-        assert.include(err.message, 'Test Error')
-      }
-    })
-  })
-  describe('#buildTokenTx', () => {
-    it('should build token', async () => {
-      try {
-        // Mock external dependencies.
-        sandbox
-          .stub(uut.bchjs.Utxo, 'get')
-          .resolves(mockData.mockUtxos)
-
-        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
-        const slpData = {
-          name: 'SLP Test Token',
-          ticker: 'SLPTEST',
-          documentUrl: 'https://FullStack.cash',
-          decimals: 0,
-          initialQty: 1,
-          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
-          mintBatonVout: null
-        }
-        const result = await uut.buildTokenTx(WIF, slpData)
-        assert.isString(result)
-      } catch (err) {
-        console.log(err)
-        assert.equal(true, false, 'unexpected result')
-      }
-    })
-    it('should handle errors', async () => {
-      try {
-        // Mock external dependencies.
-        sandbox
-          .stub(uut.bchjs.Utxo, 'get')
-          .throws(new Error('Test Error'))
-
-        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
-        const slpData = {
-          name: 'SLP Test Token',
-          ticker: 'SLPTEST',
-          documentUrl: 'https://FullStack.cash',
-          decimals: 0,
-          initialQty: 1,
-          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
-          mintBatonVout: null
-        }
-        const result = await uut.buildTokenTx(WIF, slpData)
-        assert.isString(result)
-      } catch (err) {
-        assert.include(err.message, 'Test Error')
-      }
-    })
-    it('throw error if WIF if not provided', async () => {
-      try {
-        await uut.buildTokenTx()
-        assert.equal(true, false, 'unexpected result')
-      } catch (err) {
-        assert.include(err.message, 'WIF must be a string')
-      }
-    })
-    it('throw error if token data if not provided', async () => {
-      try {
-        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
-
-        await uut.buildTokenTx(WIF)
-        assert.equal(true, false, 'unexpected result')
-      } catch (err) {
-        assert.include(err.message, 'slpData must be an object')
-      }
-    })
-  })
   describe('#createMutableTxid', () => {
     it('should create mutable transaction id ', async () => {
       try {
         // Mock external dependencies.
         sandbox
           .stub(uut.bchjs.Utxo, 'get')
-          .resolves(mockData.mockUtxos)
+          .resolves(mockData.mockUtxos02)
 
         sandbox
           .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
@@ -195,7 +70,9 @@ describe('#create.js', () => {
 
         const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
         const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
+
         const result = await uut.createMutableTxid(WIF, mspAddr)
+
         assert.isString(result)
       } catch (err) {
         console.log(err)
@@ -208,7 +85,7 @@ describe('#create.js', () => {
         // Mock external dependencies.
         sandbox
           .stub(uut.bchjs.Utxo, 'get')
-          .resolves(mockData.mockUtxos)
+          .resolves(mockData.mockUtxos02)
 
         sandbox
           .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
@@ -216,7 +93,9 @@ describe('#create.js', () => {
 
         const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
         const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
+
         const result = await uut.createMutableTxid(WIF, mspAddr)
+
         assert.isString(result)
       } catch (err) {
         assert.include(err.message, 'Test Error')
@@ -231,6 +110,7 @@ describe('#create.js', () => {
         assert.include(err.message, 'WIF must be a string')
       }
     })
+
     it('throw error if mspAddress if not provided', async () => {
       try {
         const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
@@ -238,6 +118,195 @@ describe('#create.js', () => {
         assert.equal(true, false, 'unexpected result')
       } catch (err) {
         assert.include(err.message, 'mspAddr must be a string')
+      }
+    })
+
+    it('should throw an error if there are not BCH UTXOs to pay for transaction', async () => {
+      try {
+        // Mock external dependencies.
+        mockData.mockUtxos.bchUtxos = []
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .resolves(mockData.mockUtxos)
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+        const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
+        await uut.createMutableTxid(WIF, mspAddr)
+
+        assert.fail('Unexpected code path')
+      } catch (err) {
+        // console.log(err)
+        assert.include(err.message, 'No BCH UTXOs found in wallet')
+      }
+    })
+  })
+
+  describe('#buildTokenTx', () => {
+    it('should build token', async () => {
+      try {
+        // Mock external dependencies.
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .resolves(mockData.mockUtxos02)
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+        const slpData = {
+          name: 'SLP Test Token',
+          ticker: 'SLPTEST',
+          documentUrl: 'https://FullStack.cash',
+          decimals: 0,
+          initialQty: 1,
+          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
+          mintBatonVout: null
+        }
+
+        const result = await uut.buildTokenTx(WIF, slpData)
+
+        assert.isString(result)
+      } catch (err) {
+        // console.log(err)
+        assert.equal(true, false, 'unexpected result')
+      }
+    })
+
+    it('should handle errors', async () => {
+      try {
+        // Mock external dependencies.
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .throws(new Error('Test Error'))
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+        const slpData = {
+          name: 'SLP Test Token',
+          ticker: 'SLPTEST',
+          documentUrl: 'https://FullStack.cash',
+          decimals: 0,
+          initialQty: 1,
+          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
+          mintBatonVout: null
+        }
+
+        const result = await uut.buildTokenTx(WIF, slpData)
+
+        assert.isString(result)
+      } catch (err) {
+        assert.include(err.message, 'Test Error')
+      }
+    })
+
+    it('throw error if WIF if not provided', async () => {
+      try {
+        await uut.buildTokenTx()
+        assert.equal(true, false, 'unexpected result')
+      } catch (err) {
+        assert.include(err.message, 'WIF must be a string')
+      }
+    })
+
+    it('throw error if token data if not provided', async () => {
+      try {
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+
+        await uut.buildTokenTx(WIF)
+
+        assert.equal(true, false, 'unexpected result')
+      } catch (err) {
+        assert.include(err.message, 'slpData must be an object')
+      }
+    })
+
+    it('should throw an error if there are not BCH UTXOs to pay for transaction', async () => {
+      try {
+        // Mock external dependencies.
+        mockData.mockUtxos.bchUtxos = []
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .resolves(mockData.mockUtxos)
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+
+        const slpData = {
+          name: 'SLP Test Token',
+          ticker: 'SLPTEST',
+          documentUrl: 'https://FullStack.cash',
+          decimals: 0,
+          initialQty: 1,
+          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
+          mintBatonVout: null
+        }
+
+        await uut.buildTokenTx(WIF, slpData)
+
+        assert.fail('Unexpected code path')
+      } catch (err) {
+        // console.log(err)
+        assert.include(err.message, 'No BCH UTXOs found in wallet')
+      }
+    })
+  })
+
+  describe('#createToken', () => {
+    it('should create token', async () => {
+      try {
+        // Mock external dependencies.
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .resolves(mockData.mockUtxos02)
+
+        sandbox
+          .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
+          .resolves(mockData.mockTxId)
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+        const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
+
+        const slpData = {
+          name: 'SLP Test Token',
+          ticker: 'SLPTEST',
+          documentUrl: 'https://FullStack.cash',
+          decimals: 0,
+          initialQty: 1,
+          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
+          mintBatonVout: null
+        }
+
+        const result = await uut.createToken(WIF, slpData, mspAddr)
+
+        assert.isString(result)
+      } catch (err) {
+        console.log(err)
+        assert.equal(true, false, 'unexpected result')
+      }
+    })
+
+    it('should handle errors', async () => {
+      try {
+        // Mock external dependencies.
+        sandbox
+          .stub(uut.bchjs.Utxo, 'get')
+          .resolves(mockData.mockUtxos02)
+
+        sandbox
+          .stub(uut.bchjs.RawTransactions, 'sendRawTransaction')
+          .throws(new Error('Test Error'))
+
+        const WIF = 'KxseNvKfKdMRgrMuWS5SZWHs8pjev6qJ29z9k7i5zqUDbESvxdnu'
+        const mspAddr = 'bitcoincash:qznchwd2rd2vskd4leewdah4wcjgkv33eqss59vhv6'
+
+        const slpData = {
+          name: 'SLP Test Token',
+          ticker: 'SLPTEST',
+          documentUrl: 'https://FullStack.cash',
+          decimals: 0,
+          initialQty: 1,
+          documentHash: 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831',
+          mintBatonVout: null
+        }
+        const result = await uut.createToken(WIF, slpData, mspAddr)
+        assert.isString(result)
+      } catch (err) {
+        assert.include(err.message, 'Test Error')
       }
     })
   })
--- test/unit/mocks/create-mocks.js
+++ test/unit/mocks/create-mocks.js
@@ -26,7 +26,52 @@ const mockUtxos = [
 
 const mockTxId = 'ec1d3c080759dfc7a5e29e5132230c2359aff2024d68ddf0ae1ba41c9e234831'
 
+const mockUtxos02 = {
+  address: 'bitcoincash:qqc3pqztxxcq7hr5g7f5us477rhxl96m65lp7cszfl',
+  bchUtxos: [
+    {
+      height: 0,
+      tx_hash: '8f1b9d921c02c63826bb287a50c9561bc5ee44c9e17e83c63e9bfc42f331a0e6',
+      tx_pos: 3,
+      value: 335680,
+      txid: '8f1b9d921c02c63826bb287a50c9561bc5ee44c9e17e83c63e9bfc42f331a0e6',
+      vout: 3,
+      address: 'bitcoincash:qqc3pqztxxcq7hr5g7f5us477rhxl96m65lp7cszfl',
+      isSlp: false
+    }
+  ],
+  slpUtxos: {
+    type1: {
+      tokens: [
+        {
+          height: 715677,
+          tx_hash: 'db4fcd1937edb37e035c936a22ebe0fb6f879c1d89c21dd73326581b3af61826',
+          tx_pos: 1,
+          value: 546,
+          txid: 'db4fcd1937edb37e035c936a22ebe0fb6f879c1d89c21dd73326581b3af61826',
+          vout: 1,
+          isSlp: true,
+          type: 'token',
+          qty: '1',
+          tokenId: 'db4fcd1937edb37e035c936a22ebe0fb6f879c1d89c21dd73326581b3af61826',
+          address: 'bitcoincash:qqc3pqztxxcq7hr5g7f5us477rhxl96m65lp7cszfl',
+          ticker: 'MT01',
+          name: 'Mutable Test 01',
+          documentUri: 'https://FullStack.cash',
+          documentHash: '��\u001bu�Ȱ�Ρ��:�\u0013����K�q�V\u001c�\r+:���',
+          decimals: 0,
+          qtyStr: '1'
+        }
+      ],
+      mintBatons: []
+    },
+    nft: {}
+  },
+  nullUtxos: []
+}
+
 module.exports = {
   mockUtxos,
-  mockTxId
+  mockTxId,
+  mockUtxos02
 }
