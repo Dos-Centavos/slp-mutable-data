0ed1d363f8a35d13e272238c3f07bcfaceb27695
Chris Troutner <chris.troutner@gmail.com>
2023-06-04 11:02:39 -0700

    fix(p2wdb): Updating to v2.4.1

--- examples/05-get-mutable-data.js
+++ examples/05-get-mutable-data.js
@@ -9,7 +9,7 @@ const BchWallet = require('minimal-slp-wallet')
 const { SlpMutableData } = require('../index')
 
 // const tokenId = '0de866f9fda1e667f20540a6c660a86b71cc57982b5be232459c44899c437ffc'
-const tokenId = '5c8cb997cce61426b7149a74a3997443ec7eb738c5c246d9cfe70185a6911476'
+const tokenId = '250025cf796c371aa4f7e841cc5b67312d48ce1a33a0a504fbb6c678aa8a376e'
 
 async function getMutableData () {
   try {
